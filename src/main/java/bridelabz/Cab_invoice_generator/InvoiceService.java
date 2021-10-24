78634f5fc5f2f36cc3c735cfc235d2cf5da8cedd
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 03:46:01 +0530

    Added:Invoice generator should return the total fare of the journey

--- src/main/java/bridelabz/Cab_invoice_generator/InvoiceService.java
+++ src/main/java/bridelabz/Cab_invoice_generator/InvoiceService.java
@@ -2,9 +2,16 @@ package bridelabz.Cab_invoice_generator;
 
 public class InvoiceService {
 
-	public static void main(String[] args) {
-		System.out.println("cab invoice generator");
+	private static final double COST_PER_KM = 10;
+    private static final double COST_PER_MIN = 1;
+    private static final double MIN_FARE = 5.0;
 
-	}
-
+    // Created a method to calculate the fare of journey
+    public double CalculateFare(double distance, int time) {
+        double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
+        if (totalFare < MIN_FARE) {
+            return MIN_FARE;
+        }
+        return totalFare;
+    }
 }
