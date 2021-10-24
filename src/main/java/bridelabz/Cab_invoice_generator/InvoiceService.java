5338f4e7bc73186ff8963061e20f66af24bcdc5a
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-25 03:37:41 +0530

    Added:Cab invoice generator

---
+++ src/main/java/bridelabz/Cab_invoice_generator/InvoiceService.java
@@ -0,0 +1,10 @@
+package bridelabz.Cab_invoice_generator;
+
+public class InvoiceService {
+
+	public static void main(String[] args) {
+		System.out.println("cab invoice generator");
+
+	}
+
+}
---
+++ src/test/java/bridelabz/Cab_invoice_generator/InvoiceServiceTest.java
@@ -0,0 +1,14 @@
+package bridelabz.Cab_invoice_generator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
